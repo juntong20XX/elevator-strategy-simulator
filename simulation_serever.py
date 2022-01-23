e4a8907be13fbc66ea26834b5e8e77cc9d5f88f2
Juntong <juntong_2021@outlook.com>
2022-01-23 10:56:34 +0800

    临时上传

---
+++ simulation_serever.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 23 09:52:49 2022
+
+@author: Jessi
+"""
+
+import socketserver
+
+class TcpHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        """"""
+        
